738b6a75e591cf1fd6fc697ec53cf687188ef2d5
VinzR-2159109 <vinz.roosen@student.uhasselt.be>
2023-11-20 23:59:47 +0100

    Add mutate

--- SocialRunningTracker_GraphQL_Graphene&Python/SocialRunningTracker.py
+++ SocialRunningTracker_GraphQL_Graphene&Python/SocialRunningTracker.py
@@ -4,33 +4,66 @@ from graphql_server.flask import GraphQLView
 import pandas as pd
 
 dfUsers = pd.read_excel("users.xlsx")
+dfRunningGroups = pd.read_excel("runningGroups.xlsx")
+
+class RunningGroup(ObjectType):
+    runningGroupName = Field(String)
+    id = Field(Int)
+    users = List(lambda: User)
+
+    def resolve_users(parent, info):
+        return dfUsers[dfUsers["runningGroupId"] == parent.get("id")].to_dict(orient="records")
 
 class User(ObjectType):
     userName = Field(String)
     favRunningSite = Field(String)
-    runningGroupId = Field(Int)
+    runningGroup = Field(RunningGroup)
+    age = Field(Int)
+
+    def resolve_runningGroup(parent, info):
+        return dfRunningGroups[dfRunningGroups["id"] == parent.get("runningGroupId")].to_dict(orient="records")[0]
+
+class NewUserInput(InputObjectType):
+    userName = String(required=True)
+    favRunningSite = String(required=True)
+    age = Int(required=True)
+    runningGroupId = Int(required=True)
+
+class CreateUser(Mutation):
+    class Arguments:
+        user_data = NewUserInput(required=True)
+
+    success = String()
+    user = Field(lambda: User)
+
+    def mutate(self, info, user_data):
+        new_user = {
+            "userName": user_data.userName,
+            "favRunningSite": user_data.favRunningSite,
+            "age": user_data.age,
+            "runningGroupId": user_data.runningGroupId  
+        }
+        global dfUsers
+        new_user["id"] = len(dfUsers) + 1
+        dfUsers = pd.concat([dfUsers, pd.DataFrame([new_user])], ignore_index=True)
+
+        return CreateUser(success="User created successfully", user=new_user)
+
+class Mutation(ObjectType):
+    create_user = CreateUser.Field()
 
 class Query(ObjectType):
     get_user_by_id = Field(User, id=ID(required=True))
     get_user_by_username = Field(User, username=String(required=True))
-    get_users_by_fav_running_site = List(User, fav_running_site=String(required=True))
-    get_users_in_group = List(User, running_group_id=Int(required=True))
 
     def resolve_get_user_by_id(parent, info, id):
         users = dfUsers[dfUsers["id"] == int(id)]
         return users.iloc[0]
+
     def resolve_get_user_by_username(parent, info, username):
         users = dfUsers[dfUsers["userName"] == username]
         return users.iloc[0]
-
-    def resolve_get_users_by_fav_running_site(parent, info, fav_running_site):
-        users = dfUsers[dfUsers["favRunningSite"] == fav_running_site]
-        return users.to_dict(orient="records")
-
-    def resolve_get_users_in_group(parent, info, running_group_id):
-        users = dfUsers[dfUsers["runningGroupId"] == running_group_id]
-        return users.to_dict(orient="records")
-    
-schema = Schema(query=Query)
+        
+schema = Schema(query=Query, mutation=Mutation)
 myWebApp = Flask("My App")
 myWebApp.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True))
